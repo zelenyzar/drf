c4de81842bbf191b31123aa7302aa4bf9ccfea82
antonina p <zelenyzar@gmail.com>
2026-08-23 05:31:03 +0300

    added new apps users and materials, CRUD Viewsets and Generics used

---
+++ materials/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class Course(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Название курса",
+        help_text="Укажите название курса",
+    )
+    preview = models.ImageField(
+        upload_to="course/preview",
+        verbose_name="Превью (Изображение)",
+        blank=True,
+        null=True,
+    )
+    description = models.TextField(blank=True, null=True, verbose_name="Описание курса")
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Название урока",
+        help_text="Укажите название урока",
+    )
+    description = models.TextField(blank=True, null=True, verbose_name="Описание урока")
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        related_name="lessons",
+        verbose_name="Курс",
+        blank=True,
+        null=True,
+    )
+    preview = models.ImageField(
+        upload_to="lesson/preview",
+        verbose_name="Превью (Изображение)",
+        blank=True,
+        null=True,
+    )
+    video_url = models.URLField(max_length=1000, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
--- users/models.py
+++ users/models.py
@@ -1,5 +1,41 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
-    pass
+    username = None
+
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Телефон",
+        blank=True,
+        null=True,
+        help_text="Введите номер телефона",
+    )
+    city = models.CharField(
+        max_length=56,
+        verbose_name="Город",
+        blank=True,
+        null=True,
+        help_text="Введите город",
+    )
+    avatar = models.ImageField(
+        upload_to="user/avatars/",
+        verbose_name="Аватар",
+        blank=True,
+        null=True,
+        help_text="Загрузите свой аватар",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
