a32e345981aff0b9d12b610e36432da11b5afd5b
antonina p <zelenyzar@gmail.com>
2026-08-24 04:36:31 +0300

    Task 1

--- materials/serializer.py
+++ materials/serializer.py
@@ -1,12 +1,30 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from materials.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    lessons_count = SerializerMethodField()
+
     class Meta:
         model = Course
         fields = "__all__"
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
+
+
+class CourseDetailSerializer(ModelSerializer):
+    lessons_count = SerializerMethodField()
+
+    class Meta:
+        model = Course
+        fields = ('title', 'description', 'lessons_count')
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
+
+
 
 
 class LessonSerializer(ModelSerializer):
--- materials/views.py
+++ materials/views.py
@@ -4,12 +4,16 @@ from rest_framework.generics import (CreateAPIView, DestroyAPIView,
 from rest_framework.viewsets import ModelViewSet
 
 from materials.models import Course, Lesson
-from materials.serializer import CourseSerializer, LessonSerializer
+from materials.serializer import CourseSerializer, LessonSerializer, CourseDetailSerializer
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
-    serializer_class = CourseSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return CourseDetailSerializer
+        return CourseSerializer
 
 
 class LessonCreateApiView(CreateAPIView):
@@ -20,6 +24,12 @@ class LessonCreateApiView(CreateAPIView):
 class LessonListApiView(ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+    def get_queryset(self):
+        course_id = self.request.query_params.get("course")
+        if course_id:
+            return Lesson.objects.filter(course_id=course_id)
+        return Lesson.objects.all()
 
 
 class LessonRetrieveApiView(RetrieveAPIView):
