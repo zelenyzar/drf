18cf06f077db18776357645b23adfbdf1a619fbf
antonina p <zelenyzar@gmail.com>
2026-09-10 05:07:55 +0300

    added group permissions for moders and users

--- users/permissions.py
+++ users/permissions.py
@@ -1,6 +1,16 @@
 from rest_framework import permissions
 
+
 class IsModer(permissions.BasePermission):
+    """Проверяет, является ли пользователь модератором."""
 
     def has_permission(self, request, view):
         return  request.user.groups.filter(name="moders").exists()
+
+class IsOwner(permissions.BasePermission):
+    """Проверяет, является ли пользователь владельцем."""
+
+    def has_object_permission(self, request, view, obj):
+        if obj.owner == request.user:
+            return True
+        return False
